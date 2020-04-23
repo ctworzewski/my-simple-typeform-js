1b513ddcc8caeb0628666b7e1fa54f106d93881d
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2020-04-24 00:52:47 +0200

    Add style to button and display alert in project

--- main.js
+++ main.js
@@ -12,9 +12,18 @@ let $btnNext = document.querySelector('.btnNext');
 let numberQuestion = 0;
 
 function changeQuestions() {
-
+    let $alert = document.querySelector('.alert');
     $question.textContent = typeformList[numberQuestion++];
+    if (numberQuestion > typeformList.length) {
+        console.log('Nie ma więcej pytań');
+        $alert.textContent = 'Nie ma więcej pytań';
+        $alert.style.color = 'red';
 
+    } else {
+        console.log('następne pytanie');
+        $alert.textContent = 'następne pytanie';
+        $alert.style.color = 'green';
+    }
 }
 
 $btnNext.addEventListener('click', changeQuestions);
