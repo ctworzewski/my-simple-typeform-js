4f0d35d6641161a7c2413af907f92671a7a0ec5a
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2020-04-27 05:23:21 +0200

    check index array

--- main.js
+++ main.js
@@ -6,15 +6,38 @@ let $btnPrev = document.querySelector('.btnPrev');
 
  */
 
-const tab = ['element 1', 'element 2', 'element 3'];
 
-
-
-const tab1 = new Array();
+/* const tab1 = new Array();
 tab1[0] = 'element 1';
 tab1[1] = 'element 2';
 tab1[2] = 'element 3';
 tab1[3] = 'element 4';
-
 tab1.push(60);
-tab1[10] = 'ciasteczko';
+tab1[10] = 'ciasteczko'; */
+
+/* function showQuestion() {
+    let suma = 2 + 3;
+    return suma;
+}
+
+function checkShow() {
+    let returnShowQuestion = showQuestion();
+    returnShowQuestion += 4
+    console.log(returnShowQuestion);
+} */
+
+
+const tab = ['element 1', 'element 2', 'element 3'];
+
+// funkcja sprawdza czy istnieje dany element w tablicy
+// jeśli istnieje to go pokazuje
+function showQuestion(indexArray) {
+    let $question = document.querySelector('.question');
+    if (indexArray <= tab.length - 1 && indexArray >= 0) {
+        $question.textContent = tab[indexArray];
+    } else {
+        console.log('Nie ma takiego indeksu tablicy');
+    }
+}
+
+showQuestion(2);
